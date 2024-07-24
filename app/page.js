35ec98df65b820f7311c7dc22505ac069da6422d
Jinny007 <leongjiayan@gmail.com>
2024-07-24 17:43:13 +0800

    First commit

---
+++ app/page.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+
+      <><header></header><main></main></>
+      {/* <h1 style={{ color: "white", textAlign: "center" }}>
+        Time to get started!
+      </h1>
+      <p>
+        <Link href="/meals">Meals</Link>
+      </p>
+      <p>
+        <Link href="/meals/share">Meals share</Link>
+      </p>
+      <p>
+        <Link href="/community">Community</Link>
+      </p>
+      <p>
+        <Link href="/meals/meal-1">Meal 1</Link>
+      </p>
+      <p>
+        <Link href="/meals/meal-2">Meal 2</Link>
+      </p> */}
+ 
+  );
+}
